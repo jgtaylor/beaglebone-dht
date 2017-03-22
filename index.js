fc3a54e8d941569aefc299720fbaa364f3f4055d
jgtaylor <sonicbphuct@gmail.com>
2017-03-22 20:16:04 +0100

    change args format/count - dht.read(pin, sensorModel)

--- index.js
+++ index.js
@@ -4,46 +4,66 @@ var dht = require('bindings')('beaglebone-dht'),
 
 
 module.exports = {
-    sensor: function(model) {
-        this.sensorNumber = _.result(_.find(data.sensors, {model: model}), 'number');
-        
-        return this.sensorNumber;
-    },
-
-    read: function(gpio) {
+    read: function(gpio, sensorModel) {
         if (_.isNull(gpio) || _.isUndefined(gpio)) {
             throw new Error('No GPIO pin provided!');
-        }
-        else {
-            if (!_.some(data.pinToGpio, {gpio: gpio})) {
+        } else {
+            if (!_.some(data.pinToGpio, {
+                    gpio: gpio
+                })) {
                 throw new Error('Pin must be a valid GPIO identifier!');
-            }
-            else {
+            } else {
+                switch (sensorModel) {
+                    case "DHT22":
+                        {
+                            this.sensorNumber = 22;
+                            break;
+                        }
+                    case "DHT11":
+                        {
+                            this.sensorNumber = 11;
+                            break;
+                        }
+                    case "AM2302":
+                        {
+                            this.sensorNumber = 22;
+                            break;
+                        }
+                    default:
+                        {
+                            this.sensorNumber = 22;
+                            break;
+                        }
+                }
                 return getReading(gpio, this.sensorNumber);
             }
         }
 
         function getReading(gpio, sensorNumber) {
-            var convert = _.pick(_.find(data.pinToGpio, {gpio: gpio}), ['base', 'number']);
+            var convert = _.pick(_.find(data.pinToGpio, {
+                gpio: gpio
+            }), ['base', 'number']);
             var retryCount = 0;
 
             if (!_.isUndefined(sensorNumber)) {
-                while (retryCount !== 5) {
+                while (retryCount !== 10) {
                     var current = dht.read(sensorNumber, convert.base, convert.number),
                         fahrenheit = (current.temperature * 1.8) + 32,
                         hasErrors = module.exports.checkErrors(current.result);
 
                     if (hasErrors === false) {
                         return {
-                            celsius: current.temperature,
-                            fahrenheit: fahrenheit,
-                            humidity: current.humidity
+                            celsius: current.temperature.toPrecision(2),
+                            fahrenheit: fahrenheit.toPrecision(2),
+                            humidity: current.humidity.toPrecision(2)
                         };
                     } else {
-                        if ( retryCount === 5 ) {
-                            for ( var x = 0; x < data.sensorErrors.length; x++ ) {
-                                if ( data.sensorErrors[x].value === current.result ) {
-                                    return { error: data.sensorErrors[x].name };
+                        if (retryCount === 10) {
+                            for (var x = 0; x < data.sensorErrors.length; x++) {
+                                if (data.sensorErrors[x].value === current.result) {
+                                    return {
+                                        error: data.sensorErrors[x].name
+                                    };
                                 }
                             }
                         }
@@ -55,26 +75,33 @@ module.exports = {
             }
         }
     },
-    
-    checkErrors: function (current) {
-            var hasErrors;
 
-            if (current === _.result(_.find(data.sensorErrors, {name: 'timeout'}), 'value') ||
-                current === _.result(_.find(data.sensorErrors, {name: 'checksum'}), 'value')) {
-                hasErrors = true;
-            }
-            else if (current === _.result(_.find(data.sensorErrors, {name: 'gpio'}), 'value')) {
-                hasErrors = true;
-                throw new Error('Error accessing GPIO. Make sure program is run as root with sudo!');
-            }
-            else if (current < _.result(_.find(data.sensorErrors, {name: 'success'}), 'value')) {
-                hasErrors = true;
-                throw new Error('Error calling DHT test driver read: ' + current);
-            }
-            else if (current >= _.result(_.find(data.sensorErrors, {name: 'success'}), 'value')) {
-                hasErrors = false;
-            }
+    checkErrors: function(current) {
+        var hasErrors;
 
-            return hasErrors;
+        if (current === _.result(_.find(data.sensorErrors, {
+                name: 'timeout'
+            }), 'value') ||
+            current === _.result(_.find(data.sensorErrors, {
+                name: 'checksum'
+            }), 'value')) {
+            hasErrors = true;
+        } else if (current === _.result(_.find(data.sensorErrors, {
+                name: 'gpio'
+            }), 'value')) {
+            hasErrors = true;
+            throw new Error('Error accessing GPIO. Make sure program is run as root with sudo!');
+        } else if (current < _.result(_.find(data.sensorErrors, {
+                name: 'success'
+            }), 'value')) {
+            hasErrors = true;
+            throw new Error('Error calling DHT test driver read: ' + current);
+        } else if (current >= _.result(_.find(data.sensorErrors, {
+                name: 'success'
+            }), 'value')) {
+            hasErrors = false;
         }
+
+        return hasErrors;
+    }
 };
